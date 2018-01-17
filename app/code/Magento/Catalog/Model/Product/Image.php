9f588ed1f5703d5807314c8b0b13ab430cea5966
Stanislav Lopukhov <slopukhov@magento.com>
2018-01-17 10:53:36 +0200

    MAGETWO-84480: Add cache for getimagesize() function for product images

--- app/code/Magento/Catalog/Model/Product/Image.php
+++ app/code/Magento/Catalog/Model/Product/Image.php
@@ -805,6 +805,7 @@ class Image extends \Magento\Framework\Model\AbstractModel
         $this->_mediaDirectory->delete($directory);
 
         $this->_coreFileStorageDatabase->deleteFolder($this->_mediaDirectory->getAbsolutePath($directory));
+        $this->clearImageInfoFromCache();
     }
 
     /**
@@ -913,7 +914,8 @@ class Image extends \Magento\Framework\Model\AbstractModel
         $imagePath = $this->cachePrefix  . $imagePath;
         $this->_cacheManager->save(
             $this->serializer->serialize($imageInfo),
-            $imagePath
+            $imagePath,
+            [$this->cachePrefix]
         );
     }
 
@@ -932,5 +934,15 @@ class Image extends \Magento\Framework\Model\AbstractModel
         } else {
             return $this->serializer->unserialize($cacheData);
         }
+    }
+
+    /**
+     * Clear image data from cache
+     *
+     * @return void
+     */
+    private function clearImageInfoFromCache()
+    {
+        $this->_cacheManager->clean([$this->cachePrefix]);
     }
 }
